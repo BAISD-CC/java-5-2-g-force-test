bb36e24530259828fcd89fe93083cfcc2e0d0028
Justin Kruskie <justin@kruskie.net>
2024-09-10 11:35:25 -0400

    Create GForceTest.java

---
+++ GForceTest.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class GForceTest {
+
+    /**
+    The calcAverage method calculates the average of five scores.
+    @param score1 The first score.
+    @param score2 The second score.
+    @param score3 The third score.
+    @param score4 The fourth score.
+    @param score5 The fifth score.
+    @return The average of the five scores.
+    */
+
+
+  /**
+    The determineGrade method determines the grade
+    for a numeric score.
+    @param score The numeric score.
+    @return The grade.
+    */
+
+      // String hold the grade
+
+      // Determine and the grade based on score.
+
+    public static void main(String[] args) {
+        // Variables for scores and average.
+
+        // Keyboard scanner
+
+        // Get each of the scores
+
+        // Get the average using the calcAverage method you created above
+
+        // Display the letter grades, the average, and an overall grade.
+        System.out.println("Here are the grades and the average:");
+        System.out.println("Score 1: " + determineGrade(score1));
+        System.out.println("Score 2: " + determineGrade(score2));
+        System.out.println("Score 3: " + determineGrade(score3));
+        System.out.println("Score 4: " + determineGrade(score4));
+        System.out.println("Score 5: " + determineGrade(score5));
+        System.out.printf("Average score: %.2f\n", average);
+        System.out.printf("Average Grade: %s\n",
+                determineGrade(average));
+
+        // What's the last thing you should always do in your main method?
+
+    }
+
+}
